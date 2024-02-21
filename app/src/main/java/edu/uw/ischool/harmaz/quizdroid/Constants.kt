9b6dbdfd296fba60e2ca524344f0b15bef6c00e2
harmaz <you@example.com>
2024-02-20 20:13:42 -0800

    not working yet

--- app/src/main/java/edu/uw/ischool/harmaz/quizdroid/Constants.kt
+++ app/src/main/java/edu/uw/ischool/harmaz/quizdroid/Constants.kt
@@ -14,3 +14,12 @@ const val MATH = "Math"
 const val PHYSICS = "Physics"
 const val MARVEL = "Marvel Super Heroes"
 const val SUITS = "Suits"
+
+// PREFERENCE CONSTANTS
+const val PREFS = "prefs"
+const val PREF_URL = "pref_url"
+const val PREF_INTERVAL = "pref_interval"
+const val DEFAULT_URL = "http://tednewardsandbox.site44.com/questions.json"
+const val DEFAULT_INTERVAL = 60
+const val DEFAULT_DATA_REL_PATH = "/Android/data/edu.uw.ischool.harmaz.quizdroid/questions.json"
+
